896f598e5fe8422c214a331b7893bc773e654365
Your Name <mariamfarid820@gmail.com>
2025-08-24 23:45:52 +0300

    Add unit tests for SimpleCalculator class

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -2,25 +2,29 @@ import unittest
 from simple_calculator import SimpleCalculator
 
 class TestSimpleCalculator(unittest.TestCase):
-
     def setUp(self):
         self.calc = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(-2, -3), -5)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(5, 3), 2)
         self.assertEqual(self.calc.subtract(0, 5), -5)
+        self.assertEqual(self.calc.subtract(-3, -2), -1)
 
     def test_multiply(self):
-        self.assertEqual(self.calc.multiply(4, 3), 12)
+        self.assertEqual(self.calc.multiply(2, 3), 6)
         self.assertEqual(self.calc.multiply(-1, 5), -5)
+        self.assertEqual(self.calc.multiply(0, 100), 0)
 
     def test_divide(self):
         self.assertEqual(self.calc.divide(10, 2), 5)
-        self.assertIsNone(self.calc.divide(5, 0))
+        self.assertEqual(self.calc.divide(-9, 3), -3)
+        with self.assertRaises(ZeroDivisionError):
+            self.calc.divide(5, 0)
 
 if __name__ == "__main__":
     unittest.main()
